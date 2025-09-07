a7612f7ee2f901f71629eabdd94411cadc0ffac7
fcrescio <francesco.crescioli@lpnhe.in2p3.fr>
2025-09-07 11:44:38 +0200

    Release MediaPlayer after playback

--- app/src/main/java/li/crescio/penates/diana/player/AndroidPlayer.kt
+++ app/src/main/java/li/crescio/penates/diana/player/AndroidPlayer.kt
@@ -6,12 +6,19 @@ import android.media.MediaPlayer
 class AndroidPlayer : Player {
     override fun play(filePath: String) {
         val player = MediaPlayer()
+        var started = false
         try {
             player.setDataSource(filePath)
+            player.setOnCompletionListener { it.release() }
             player.prepare()
             player.start()
+            started = true
         } catch (e: Exception) {
             // Swallow exceptions for this simplified implementation
+        } finally {
+            if (!started) {
+                player.release()
+            }
         }
     }
 }
